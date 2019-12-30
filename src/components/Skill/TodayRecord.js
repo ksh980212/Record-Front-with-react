d34cb0fdb7ce5ae9cebbc51e969bb54af71e1938
ksh980212 <ksh980212@gmail.com>
2019-12-30 20:39:22 +0900

    DELETE api/v1/record insert

--- src/components/Skill/TodayRecord.js
+++ src/components/Skill/TodayRecord.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Toast, ToastBody, ToastHeader} from 'reactstrap';
+import { Button, Toast, ToastBody, ToastHeader} from 'reactstrap';
 import axios from 'axios';
 
 
@@ -8,17 +8,27 @@ const TodayRecord = () => {
 
     const [data, setData] = useState([]);
 
-    useEffect( () => {
+    const load= () => {
         axios.get("http://localhost:8080/api/v1/record?sort=id,desc")
         .then((response)=>{
             setData(response.data);
-        }).catch(()=>{
         })
+    }
+
+    useEffect( () => {
+        load()
     }, [])
+
+    const deleteRecord = param => () =>{
+        axios.delete(`http://localhost:8080/api/v1/record?id=${param}`)
+        .then(()=>{
+            load()
+        })
+    }
 
     const recordList = data.map(todo => {
         return(
-            <div key ={todo.key} className="p-3 my-2 rounded">
+            <div key ={todo.id} className="p-3 my-2 rounded">
                 <Toast id="todoList-toast">
                     <ToastHeader icon="primary">
                         {todo.title}
@@ -26,6 +36,7 @@ const TodayRecord = () => {
                     <ToastBody>
                         {todo.content}
                         <p>{todo.createdDate.split("T")[0] +" "+ todo.createdDate.split("T")[1].substring(0, 8)}</p>
+                        <Button onClick={deleteRecord(todo.id)} close></Button>
                     </ToastBody>
                 </Toast>
             </div>
