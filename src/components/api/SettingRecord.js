122c24585940e22e6e8eda091c9dc3e326c93258
ksh980212 <ksh980212@gmail.com>
2019-12-30 23:49:44 +0900

    초기 화면 구축

--- src/components/api/SettingRecord.js
+++ src/components/api/SettingRecord.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Toast, ToastHeader, ToastBody, Button,  Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import {Col, Toast, ToastHeader, ToastBody, Button,  Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import axios from 'axios';
 
 
@@ -7,6 +7,7 @@ const SettingRecord = () => {
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [color, setColor]= useState('primary');
 
     const titleOnChange = event=>{
         setTitle(event.target.value);
@@ -15,11 +16,16 @@ const SettingRecord = () => {
     const contentOnChange = event =>{
         setContent(event.target.value);
     }
+    
+    const colorOnChange = event => {
+        setColor(event.target.value);
+    }
 
     const saveRecord = () => {
         axios.post("http://localhost:8080/api/v1/record",{
             title: title,
-            content: content
+            content: content,
+            iconColor : color
         }).then(()=>{
             alert("저장이 완료되었습니다");
             setTitle("");
@@ -32,7 +38,7 @@ const SettingRecord = () => {
     return (
         <div>
             <h3 id="setting-record-title">Record your record</h3>
-            <Form>
+            <Form onSubmit ={saveRecord}>
                 <FormGroup>
                     <Label>Title</Label>
                     <Input onChange={titleOnChange} value={title}/>
@@ -41,13 +47,42 @@ const SettingRecord = () => {
 
                 <FormGroup>
                     <Label for="exampleEmail">Content</Label>
-                    <Input onChange={contentOnChange} value={content}/>
+                    <Input onChange={contentOnChange} value={content} />
                     <FormText>Check the preview at the bottom.</FormText>
+                </FormGroup>
+
+                <FormGroup tag="fieldset" row>
+                    <Col sm={10}>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio2" onChange={colorOnChange}  value="primary" />
+                            <ToastHeader icon="primary" />
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio2" onChange={colorOnChange}  value="success" />
+                            <ToastHeader icon="success" />
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check disabled>
+                        <Label check>
+                            <Input type="radio" name="radio2" onChange={colorOnChange} value="warning" />
+                            <ToastHeader icon="warning" />
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check disabled>
+                        <Label check>
+                            <Input type="radio" name="radio2" onChange={colorOnChange} value="info" />
+                            <ToastHeader icon="info" />
+                        </Label>
+                    </FormGroup>
+                    </Col>
                 </FormGroup>
 
                 <div className = "preview-record">
                     <Toast id="todoList-toast">
-                        <ToastHeader icon="primary">
+                        <ToastHeader icon={color}>
                             {title}
                         </ToastHeader>
                         <ToastBody id ="preview-toast-body">
--- src/components/api/TodayRecord.js
+++ src/components/api/TodayRecord.js
@@ -11,7 +11,20 @@ const TodayRecord = () => {
     const load= () => {
         axios.get("http://localhost:8080/api/v1/record?sort=id,desc")
         .then((response)=>{
+
+            if(response.data.length === 0){
+                setData(
+                    [
+                        {id:0, title:"Hello", content:"Welcome to Record App", createdDate: "2019-12-30T12:30", iconColor: "primary"},
+                        {id:1, title:"How to use Record?", content: "Go to Setting -> Record Setting", createdDate:"2019-12-30T12:30", iconColor:"success"},
+                        {id:2, title:"How to use Record?", content: "And you can record your record", createdDate:"2019-12-30T12:30", iconColor:"warning"},
+                        {id:3, title:"Contact me", content: "ksh980212@gmail.com", createdDate:"2019-12-30T12:30", iconColor:"info"}
+                    ]
+                    )
+            }
+            else{
             setData(response.data);
+            }
         })
     }
 
@@ -30,12 +43,12 @@ const TodayRecord = () => {
         return(
             <div key ={todo.id} className="p-3 my-2 rounded">
                 <Toast id="todoList-toast">
-                    <ToastHeader icon="primary">
+                    <ToastHeader icon={todo.iconColor}>
                         {todo.title}
                     </ToastHeader>
-                    <ToastBody>
+                    <ToastBody id = "toast-body">
                         {todo.content}
-                        <p>{todo.createdDate.split("T")[0] +" "+ todo.createdDate.split("T")[1].substring(0, 8)}</p>
+                        <p className = "created-date-font">{todo.createdDate.split("T")[0] +" "+ todo.createdDate.split("T")[1].substring(0, 8)}</p>
                         <Button onClick={deleteRecord(todo.id)} close></Button>
                     </ToastBody>
                 </Toast>
